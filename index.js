319f4428674c3a9d5b97e65452df554612edf738
Toufique18 <toufiqueeamam7@gmail.com>
2024-06-12 16:52:05 +0600

    user information display in dashboard from  server and commit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 //console.log(process.env.DB_USER)
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mhvsuxa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -38,15 +38,49 @@ async function run() {
 
     //user store
     app.post("/users", async (req, res) => {
-      const user = req.body;
-      const result = await usersCollection.insertOne(user);
-      res.send(result);
-  });
+      const { email } = req.body;
+      const existingUser = await usersCollection.findOne({ email });
 
-    app.get("/contest_info", async (req, res)=> {
-        const coursor = contestCollection.find();
-        const result = await coursor.toArray()
-        res.send(result)
+      if (existingUser) {
+        res.status(200).send({ message: 'User already exists' });
+      } else {
+        const result = await usersCollection.insertOne(req.body);
+        res.status(201).send(result);
+      }
+    });
+    //user role
+    app.patch("/users/:id/role", async (req, res) => {
+      const { id } = req.params;
+      const { role } = req.body;
+      const result = await usersCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: { role } }
+      );
+      res.send(result);
+    });
+    //user delete
+    app.delete("/users/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await usersCollection.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
+    // Add route to fetch users
+app.get("/users", async (req, res) => {
+  try {
+    const users = await usersCollection.find().toArray();
+    res.json(users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ error: "Error fetching users" });
+  }
+});
+
+
+    app.get("/contest_info", async (req, res) => {
+      const coursor = contestCollection.find();
+      const result = await coursor.toArray()
+      res.send(result)
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -60,9 +94,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Contesthub server is running')
+  res.send('Contesthub server is running')
 })
 
 app.listen(port, () => {
-    console.log(`Contesthub server is running on port: ${port}`)
+  console.log(`Contesthub server is running on port: ${port}`)
 })
